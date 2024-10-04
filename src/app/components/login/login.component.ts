8d92f4b0a9aed9e0ef580b2add03c4275e01b124
youknowmae <ganaangelmae@gmail.com>
2024-10-04 21:49:20 +0800

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GeneralService } from '../../services/general.service';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,49 +13,49 @@ export class LoginComponent {
 
   isLoggingIn: boolean = false;
   
-  // loginForm: FormGroup 
-  // constructor(
-  //   private gs: GeneralService,
-  //   private as: AuthService,
-  //   private fb: FormBuilder,
-  // ) {
-  //   this.loginForm =  this.fb.group({
-  //     email: ['', [Validators.required, Validators.maxLength(30)]],
-  //     password: ['', [Validators.required, Validators.maxLength(30)]]
-  //   })
+  loginForm: FormGroup 
+  constructor(
+    private gs: GeneralService,
+    private as: AuthService,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm =  this.fb.group({
+      email: ['', [Validators.required, Validators.maxLength(30)]],
+      password: ['', [Validators.required, Validators.maxLength(30)]]
+    })
 
 
-  // }
+  }
   togglePassVisibility() {
     this.visible = !this.visible;
   }
 
-  // login() {
-  //   if(this.isLoggingIn) {
-  //     return
-  //   }
+  login() {
+    if(this.isLoggingIn) {
+      return
+    }
 
-  //   this.isLoggingIn = true
+    this.isLoggingIn = true
 
-  //   this.as.login(this.loginForm.value).subscribe(
-  //     response => {
-  //       this.isLoggingIn = false
-  //       this.gs.successToastAlert('Login Successful!')
-  //     },
-  //     error => {
-  //       console.error(error)
-  //       if(error.status === 0) {
-  //         this.gs.errorAlert('Oops!', 'Something went wrong. Please try again later.')
-  //       }
-  //       else if(error.status === 401) {
-  //         this.gs.errorAlert(error.error.title, error.error.message)
-  //       }
-  //       else if(error.status === 403) {
-  //         this.gs.errorAlert(error.error.title, error.error.message)
-  //       }
+    this.as.login(this.loginForm.value).subscribe(
+      response => {
+        this.isLoggingIn = false
+        this.gs.successToastAlert('Login Successful!')
+      },
+      error => {
+        console.error(error)
+        if(error.status === 0) {
+          this.gs.errorAlert('Oops!', 'Something went wrong. Please try again later.')
+        }
+        else if(error.status === 401) {
+          this.gs.errorAlert(error.error.title, error.error.message)
+        }
+        else if(error.status === 403) {
+          this.gs.errorAlert(error.error.title, error.error.message)
+        }
 
-  //       this.isLoggingIn = false
-  //     }
-  //   )
-  // }
+        this.isLoggingIn = false
+      }
+    )
+  }
 }
