f9ff8b518a1756a36c35880d86e8301028d64486
Raveniis <128274869+Raveniis@users.noreply.github.com>
2025-05-11 19:46:02 +0800

    industry partner request update

--- src/app/main/components/newpartners/components/list/list.component.ts
+++ src/app/main/components/newpartners/components/list/list.component.ts
@@ -9,21 +9,21 @@ import { pagination } from '../../../../../model/pagination.model';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  unfilteredIndustryPartners: any = []
-  industryPartners: any = []
-  isLoading: boolean = true
-  
-  statusFilter: any = 2
-  searchFilter: string = ''
+  unfilteredIndustryPartners: any = [];
+  industryPartners: any = [];
+  isLoading: boolean = true;
+
+  statusFilter: any = 2;
+  searchFilter: string = '';
 
   pagination: pagination = <pagination>{};
 
   constructor(
-    private ds: DataService, 
-    private us: UserService, 
+    private ds: DataService,
+    private us: UserService,
     private gs: GeneralService,
     private router: Router
   ) {
@@ -34,102 +34,109 @@ export class ListComponent {
       total: 0,
       per_page: 15,
       last_page: 0,
-    }
+    };
   }
 
   ngOnInit() {
-    this.getIndustryPartnerRequest()
+    this.getIndustryPartnerRequest();
   }
 
   getIndustryPartnerRequest() {
     this.ds.get('superadmin/request/industryPartners').subscribe(
-      response => {
-        console.log(response)
+      (response) => {
+        console.log(response);
         let partners = response.filter(
-          (data: any) => data.status == 2 || data.status == 3 || data.status == 4
-        )
-        
-        this.unfilteredIndustryPartners = partners.map((element: any) => {
-          if(element.status == 2) {
-            element.status_text = 'For Approval'
-          }
-          else if(element.status == 3) {
-            element.status_text = 'Not Approved'
-          } 
-          else if(element.status == 4) {
-            element.status_text = 'Approved'
-          }
+          (data: any) =>
+            data.request_status == 2 || data.request_status == 3 || data.request_status == 4
+        );
 
-          return element
-          
+        this.unfilteredIndustryPartners = partners.map((item: any) => {
+          if (item.request_status == 2) {
+            item.status_text = 'For Approval';
+          } else if (item.request_status == 3) {
+            item.status_text = 'Declined';
+          } else if (item.request_status == 4) {
+            item.status_text = 'Approved';
+          }
+          return item;
         });
 
-        this.applyFilter()
-        this.isLoading = false
+        this.applyFilter();
+        this.isLoading = false;
       },
-      error => {
-        this.gs.errorAlert('Oops!', 'Something went wrong. Please try again later.')
-        console.error(error)
-        this.isLoading = false
+      (error) => {
+        this.gs.errorAlert(
+          'Oops!',
+          'Something went wrong. Please try again later.'
+        );
+        console.error(error);
+        this.isLoading = false;
       }
-    )
+    );
   }
 
   viewRequestDetails(id: number) {
-    this.us.setIndustryPartnerAddRequest(id)
-    this.router.navigate(['main/newpartners/view'])
+    this.us.setIndustryPartnerAddRequest(id);
+    this.router.navigate(['main/newpartners/view']);
   }
 
   onStatusFilterChange(event: MatSelectChange) {
-    this.statusFilter = event.value
-    this.applyFilter()
+    this.statusFilter = event.value;
+    this.applyFilter();
   }
 
   search(search: string) {
-    this.searchFilter = search.trim().toLowerCase()
-    this.applyFilter()
+    this.searchFilter = search.trim().toLowerCase();
+    this.applyFilter();
   }
 
   applyFilter() {
-    let search = this.searchFilter.toLowerCase()
+    let search = this.searchFilter.toLowerCase();
 
-    console.log('filtering')
+    console.log('filtering');
 
-    let data = this.unfilteredIndustryPartners
+    let data = this.unfilteredIndustryPartners;
 
-    if(this.statusFilter) {
-      data = data.filter(
-        (data: any) => data.status == this.statusFilter
-      )
+    if (this.statusFilter) {
+      data = data.filter((item: any) => {
+        return item.request_status == this.statusFilter;
+      });
     }
 
-    if(this.searchFilter) {
+
+    if (this.searchFilter) {
       data = data.filter((element: any) => {
-        return element.company_name.toLowerCase().includes(search) ||
+        return (
+          element.company_name.toLowerCase().includes(search) ||
           element.municipality.toLowerCase().includes(search)
-      })
+        );
+      });
     }
 
-    this.pagination = this.gs.getPaginationDetails(data, this.pagination.current_page, this.pagination.per_page)
+    this.pagination = this.gs.getPaginationDetails(
+      data,
+      this.pagination.current_page,
+      this.pagination.per_page
+    );
 
     data = data.slice(this.pagination.from, this.pagination.to);
-    this.pagination.from++
+    this.pagination.from++;
 
-    this.industryPartners = data
+    this.industryPartners = data;
   }
 
   changePage(page: number) {
-    const destination_page = this.pagination.current_page + page
-    if(destination_page < 1 || destination_page > this.pagination.last_page) {
-      return
+    const destination_page = this.pagination.current_page + page;
+    if (destination_page < 1 || destination_page > this.pagination.last_page) {
+      return;
     }
-    
-    this.pagination.current_page += page
-    this.applyFilter()
+
+    this.pagination.current_page += page;
+    this.applyFilter();
   }
 
-  jumpPage(page: number){
-    this.pagination.current_page = page
-    this.applyFilter()
+  jumpPage(page: number) {
+    this.pagination.current_page = page;
+    this.applyFilter();
   }
 }
--- src/app/main/components/newpartners/components/view/view.component.ts
+++ src/app/main/components/newpartners/components/view/view.component.ts
@@ -11,21 +11,21 @@ import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrl: './view.component.scss'
+  styleUrl: './view.component.scss',
 })
 export class ViewComponent {
-  industryPartner: any
+  industryPartner: any;
 
-  accountDetails: FormGroup
+  accountDetails: FormGroup;
 
-  showPassword: boolean = false
-  isSubmitting: boolean = false
+  showPassword: boolean = false;
+  isSubmitting: boolean = false;
 
-  file: any
-  filePreview: any
-  isImage: boolean = false
+  file: any;
+  filePreview: any;
+  isImage: boolean = false;
 
-  isLoading: boolean = true
+  isLoading: boolean = true;
 
   constructor(
     private us: UserService,
@@ -33,205 +33,242 @@ export class ViewComponent {
     private gs: GeneralService,
     private fb: FormBuilder,
     private router: Router,
-    private sanitizer: DomSanitizer,
+    private sanitizer: DomSanitizer
   ) {
     this.accountDetails = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(8)]],
-      slots: [null,[Validators.required, Validators.min(1), Validators.max(50)]]
-    })
-
+      slots: [
+        null,
+        [Validators.required, Validators.min(1), Validators.max(50)],
+      ],
+    });
   }
 
   ngOnInit() {
-    this.getAddRequest()
+    this.getAddRequest();
   }
 
   getAddRequest() {
-    let id = this.us.getIndustryPartnerAddRequest()
+    let id = this.us.getIndustryPartnerAddRequest();
 
-    if(!id) {
-      this.router.navigate(['main/newpartners/list'])
-      return
+    if (!id) {
+      this.router.navigate(['main/newpartners/list']);
+      return;
     }
 
     this.ds.get('superadmin/request/industryPartners/', id).subscribe(
-      response => {
-        console.log(response)
+      (response) => {
+        console.log(response);
 
-        let industryPartner = response
+        let industryPartner = response;
 
         let companyHead = industryPartner.company_head;
-        let fullName = `${companyHead?.first_name || ''} ${companyHead?.last_name || ''} ${companyHead?.ext_name || ''}`.trim();
+        let fullName = `${companyHead?.first_name || ''} ${
+          companyHead?.last_name || ''
+        } ${companyHead?.ext_name || ''}`.trim();
         industryPartner.company_head.full_name = fullName;
 
         let supervisor = industryPartner.immediate_supervisor;
-        let supervisorFullName = `${supervisor?.first_name || ''} ${supervisor?.last_name || ''} ${supervisor?.ext_name || ''}`.trim();
+        let supervisorFullName = `${supervisor?.first_name || ''} ${
+          supervisor?.last_name || ''
+        } ${supervisor?.ext_name || ''}`.trim();
         industryPartner.immediate_supervisor.full_name = supervisorFullName;
 
-        let full_address = `${industryPartner?.street || ''} ${industryPartner?.barangay || ''}, ${industryPartner?.municipality || ''}`
-        industryPartner.full_address = full_address
+        let full_address = `${industryPartner?.street || ''} ${
+          industryPartner?.barangay || ''
+        }, ${industryPartner?.municipality || ''}`;
+        industryPartner.full_address = full_address;
 
-        this.industryPartner = industryPartner
+        this.industryPartner = industryPartner;
 
         this.accountDetails.patchValue({
-          email: industryPartner.email
-        })
+          email: industryPartner.email,
+        });
 
-        this.isLoading = false
+        this.isLoading = false;
       },
-      error => {
-        if(error.status === 404) {
-          this.router.navigate(['main/newpartners/list'])
-          this.gs.errorAlert('Not Found!', 'The industry partner approval not found.')
-        }
-        else {
-          this.gs.errorAlert('Oops!', 'Something went wrong. Please try again later.')
+      (error) => {
+        if (error.status === 404) {
+          this.router.navigate(['main/newpartners/list']);
+          this.gs.errorAlert(
+            'Not Found!',
+            'The industry partner approval not found.'
+          );
+        } else {
+          this.gs.errorAlert(
+            'Oops!',
+            'Something went wrong. Please try again later.'
+          );
         }
 
-        console.error(error)
-        this.isLoading = false
+        console.error(error);
+        this.isLoading = false;
       }
-    )
-
+    );
   }
 
   approveConfirmation() {
     Swal.fire({
-      title: "Approve?",
-      text: "Are you sure you want to Approve this company?",
-      icon: "warning",
+      title: 'Approve?',
+      text: 'Are you sure you want to Approve this company?',
+      icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes',
       cancelButtonText: 'Cancel',
-      confirmButtonColor: "#4f6f52",
-      cancelButtonColor: "#777777",
+      confirmButtonColor: '#4f6f52',
+      cancelButtonColor: '#777777',
     }).then((result) => {
       if (result.isConfirmed) {
-        this.approvePartner()
+        this.approvePartner();
       }
     });
   }
 
   approvePartner() {
-    if(this.isSubmitting) {
-      return
+    if (this.isSubmitting) {
+      return;
     }
 
-    this.isSubmitting = true
+    this.isSubmitting = true;
 
-    const formdata = new FormData
+    const formdata = new FormData();
 
-    formdata.append('mou', this.file)
-    formdata.append('email', this.accountDetails.value.email)
-    formdata.append('password', this.accountDetails.value.password)
-    formdata.append('slots', this.accountDetails.value.slots)
+    formdata.append('mou', this.file);
+    formdata.append('email', this.accountDetails.value.email);
+    formdata.append('password', this.accountDetails.value.password);
+    formdata.append('slots', this.accountDetails.value.slots);
 
-
-    this.ds.post('superadmin/request/industryPartners/', this.industryPartner.id, formdata).subscribe(
-      response => {
-        this.isSubmitting = false
-        console.log(response)
-        this.gs.successAlert(response.title, response.message)
-        this.router.navigate(['main/newpartners/list'])
-      },
-      error => {
-        this.isSubmitting = false
-        if(error.status == 422) {
-          this.gs.errorAlert(error.error.title || 'Invalid Input!', error.error.error || 'Please check your input')
+    this.ds
+      .post(
+        'superadmin/request/industryPartners/',
+        this.industryPartner.id,
+        formdata
+      )
+      .subscribe(
+        (response) => {
+          this.isSubmitting = false;
+          console.log(response);
+          this.gs.successAlert(response.title, response.message);
+          this.router.navigate(['main/newpartners/list']);
+        },
+        (error) => {
+          this.isSubmitting = false;
+          if (error.status === 409) {
+            this.gs.errorAlert(error.error.title, error.error.message);
+          } else if (error.status == 422) {
+            this.gs.errorAlert(
+              error.error.title || 'Invalid Input!',
+              error.error.error || 'Please check your input'
+            );
+          } else {
+            this.gs.errorAlert(
+              'Oops',
+              'Something went wrong. Please try again later.'
+            );
+          }
+          console.log(error);
         }
-        else {
-          this.gs.errorAlert('Oops', 'Something went wrong. Please try again later.')
-        }
-        console.log(error)
-      }
-    )
+      );
   }
 
   downloadDocx() {
-    this.isSubmitting = true
+    this.isSubmitting = true;
 
-    this.ds.download('superadmin/request/industryPartners/mou/', this.industryPartner.id).subscribe(
-      (response: Blob) => {
-        saveAs(response, 'MOU.docx');
-        this.isSubmitting = false
-      },
-      error => {
-        this.gs.errorAlert('Error!', 'Something went wrong. Please try again later.')
-        console.error(error)
-        this.isSubmitting = false
-
-      }
-    )
+    this.ds
+      .download(
+        'superadmin/request/industryPartners/mou/',
+        this.industryPartner.id
+      )
+      .subscribe(
+        (response: Blob) => {
+          saveAs(response, 'MOU.docx');
+          this.isSubmitting = false;
+        },
+        (error) => {
+          this.gs.errorAlert(
+            'Error!',
+            'Something went wrong. Please try again later.'
+          );
+          console.error(error);
+          this.isSubmitting = false;
+        }
+      );
   }
 
   sanitizeUrl(url: string) {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   uploadFile(event: any) {
     this.file = event.target.files[0];
 
-    let file = this.file
+    let file = this.file;
 
     const fileType = file.type;
 
-      if (fileType.startsWith('image/')) {
-        this.isImage = true;
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.filePreview = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
-
-      else if (fileType === 'application/pdf') {
-        this.isImage = false;
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.filePreview = this.sanitizer.bypassSecurityTrustResourceUrl(e.target.result +  '#toolbar=0&navpanes=0&scrollbar=0');
-        };
-        reader.readAsDataURL(file);
-      }
+    if (fileType.startsWith('image/')) {
+      this.isImage = true;
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.filePreview = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    } else if (fileType === 'application/pdf') {
+      this.isImage = false;
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.filePreview = this.sanitizer.bypassSecurityTrustResourceUrl(
+          e.target.result + '#toolbar=0&navpanes=0&scrollbar=0'
+        );
+      };
+      reader.readAsDataURL(file);
     }
+  }
 
-
-    reject() {
-      Swal.fire({
-        title: "Please state the reason for declining.",
-        input: "text",
-        inputAttributes: {
-          autocapitalize: "off"
-        },
-        showCancelButton: true,
-        confirmButtonText: 'Decline',
-        cancelButtonText: 'Cancel',
-        confirmButtonColor: "#ff4141",
-        cancelButtonColor: "#777777",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          const formData = new FormData
-          formData.append('remarks', result.value)
-          this.ds.post('superadmin/request/industryPartners/reject/', this.industryPartner.id, formData).subscribe(
-            response => {
-              this.gs.successAlert(response.title, response.message)
-              this.router.navigate(['main/newpartners/list'])
-              this.industryPartner.status = 3
+  reject() {
+    Swal.fire({
+      title: 'Please state the reason for declining.',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off',
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Decline',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#ff4141',
+      cancelButtonColor: '#777777',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const formData = new FormData();
+        formData.append('remarks', result.value);
+        this.ds
+          .post(
+            'superadmin/request/industryPartners/reject/',
+            this.industryPartner.id,
+            formData
+          )
+          .subscribe(
+            (response) => {
+              this.gs.successAlert(response.title, response.message);
+              this.router.navigate(['main/newpartners/list']);
+              this.industryPartner.status = 3;
             },
-            error => {
-              console.error(error)
-              if(error.status === 409) {
-                this.gs.errorAlert(error.error.title, error.error.message)
-              }
-              else if(error.status === 422) {
-                this.gs.errorAlert(error.error.title, error.error.error)
-              }
-              else {
-                this.gs.errorAlert("Oops!", "Something went wrong. Please try again later.")
+            (error) => {
+              console.error(error);
+              if (error.status === 409) {
+                this.gs.errorAlert(error.error.title, error.error.message);
+              } else if (error.status === 422) {
+                this.gs.errorAlert(error.error.title, error.error.message);
+              } else {
+                this.gs.errorAlert(
+                  'Oops!',
+                  'Something went wrong. Please try again later.'
+                );
               }
             }
-          )
-        }
-      });
-    }
+          );
+      }
+    });
+  }
 }
